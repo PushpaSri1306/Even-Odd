cbd789b6efb6216144870df3d5980c3b83365137
PushpaSri1306 <puttapushpasri@gmail.com>
2025-01-28 20:22:48 +0530

    Even or Odd

---
+++ Even.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a = sc.nextInt();
+        String r = a%2==0?"even":"odd";
+        System.out.println(r);
+        sc.close();
+    }
+}
